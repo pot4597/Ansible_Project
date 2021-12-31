953be773499ef61e0e259a19493a355c783cb8f1
pot4597 <61907361+pot4597@users.noreply.github.com>
2021-12-31 16:36:52 +0900

    Project_20211230_v1

--- Python_Script/L3_ARP.py
+++ Python_Script/L3_ARP.py
@@ -11,6 +11,8 @@ import xml.etree.ElementTree as ET
 import sys
 from urllib import parse
 
+##보안을 위해 민감한 정보 삭제 및 테이블명/필드명 변경##
+
 ###Cisco L3 Arp Getting###
 file ='arp_summary.csv'
 
@@ -55,20 +57,6 @@ for rows in csvReader:
         val = (IPADDR, MACADDR, Device_IP, i)
         curs.execute(sql, val)
 curs.execute("update arp_temp set arp_mac = replace(arp_mac, '.', '')")
-
-conn.commit()
-f.close()
-conn.close()
-
-
-curs.execute("update arp_temp set arp_mac = replace(arp_mac, '.', '')")
-curs.execute('insert into arp (arp_ip, arp_mac, arp_switchip) select arp_ip, arp_mac, arp_switchip from arp_temp as t where t.arp_mac not in (select arp_mac from arp)')
-curs.execute('update arp as a , arp_temp as t set a.arp_ip = t.arp_ip where a.arp_mac = t.arp_mac and a.arp_ip != t.arp_ip')
-curs.execute('delete n1 from arp n1, arp n2 where n1.num < n2.num and n1.arp_mac = n2.arp_mac')
-curs.execute('alter table arp auto_increment=1')
-curs.execute('set @count = 0')
-curs.execute('update arp set num = @count:=@count+1')
-
 
 conn.commit()
 f.close()
