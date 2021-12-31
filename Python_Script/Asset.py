75ba61868588049b6d0fc3e2d1ffdc7b733653a8
pot4597 <61907361+pot4597@users.noreply.github.com>
2021-12-31 16:25:30 +0900

    Project_20211230_v1
    Project_20211230_v1

--- Python_Script/Asset.py
+++ Python_Script/Asset.py
@@ -9,14 +9,14 @@ try :
         cursor_postgres = conn_postgres.cursor()
 
         #자산 Postgres db서버 접속후 데이터 select
-        cursor_postgres.execute("select *  from mac_info where jasan_b not in ('모니터','스캐너')")
+        cursor_postgres.execute("select *  from Test_jasan")
 
         #로컬 Mysql서버 연결
         conn_mysql = pymysql.connect(host='127.0.0.1', user='***', password='***',db='***', charset='utf8')
         cursor_mysql = conn_mysql.cursor()
 
         #로컬 Mysql서버 접속후 데이터 전체 삭제
-        cursor_mysql.execute("delete from jasan_temp")
+        cursor_mysql.execute("delete from Test")
         curs = conn_mysql.cursor()
 
 
@@ -30,13 +30,12 @@ try :
         while row_jasan:
             i += 1 
             #데이터 insert
-            sql = "insert into jasan_temp (jasan_assetcode, jasan_num, jasan_site, jasan_buseo, jasan_userid, jasan_username, jasan_location, jasan_locationdetail, jasan_macaddr, num) values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+            sql = "insert into Test (jasan_assetcode, jasan_num, jasan_site, jasan_buseo, jasan_userid, jasan_username, jasan_location, jasan_locationdetail, jasan_macaddr, num) values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
             val = (row_jasan[0], row_jasan[1], row_jasan[2], row_jasan[3], row_jasan[4], row_jasan[5], row_jasan[6], row_jasan[7], row_jasan[8], i)
             curs.execute(sql, val)
 
             #자산 서버 postgres db 에서 정보 한줄 가져오기
             row_jasan = cursor_postgres.fetchone()
-            print(row_jasan)
 
 
         conn_postgres.commit()
